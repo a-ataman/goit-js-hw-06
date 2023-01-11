041be0739cb67bc157cf8a2a06c911fc8a9d9b88
Alice Atamanenko <alice_ataman@icloud.com>
2023-01-12 00:07:20 +0300

    fix

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,12 @@
-const input = document.querySelector('input');
+const input = document.querySelector('#validation-input');
 
-let inputValue = document.querySelector('#validation-input');
+input.addEventListener('blur', () => {
+    const length = input.value.length;
+    const num = Number(input.getAttribute('data-length'));
 
-let length = inputValue.dataset.length;
+    if (length === num) {
+       return input.classList.add('valid');
+    }
+        input.classList.add('invalid');
 
-
-inputValue.addEventListener('change', element =>{ 
-
-  const text = element.target.value;
-    if (text.length === +length) {
-    inputValue.classList.add('valid');
-      
-    } else {
-    inputValue.classList.add('invalid');
-  }
 });
--- js/task-08.js
+++ js/task-08.js
@@ -4,12 +4,12 @@ form.addEventListener('submit', submitHandler)
 
 function submitHandler(event) {
   event.preventDefault();
-  const formContents = { email: email.value, Password: password.value };
   const {email, password} = event.currentTarget.elements;
-
-    if (email.value === '' || password.value === '') {
-        return alert('Заповніть всі поля')
+  
+  if (email.value === '' || password.value === '') {
+    return alert('Заповніть всі поля')
   };
+  const formContents = { email: email.value, Password: password.value };
 
   console.log(formContents);
   
