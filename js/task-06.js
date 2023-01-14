e10d28e9dfdccf51efec11deb47c6ebad4385e46
Alice Atamanenko <alice_ataman@icloud.com>
2023-01-14 23:25:16 +0200

    fix task 06

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,9 @@ input.addEventListener('blur', () => {
     const length = input.value.length;
 
     if (length === numberLength) {
+        input.classList.remove('invalid');
         return input.classList.add('valid');
     }
+    input.classList.remove('valid');
     input.classList.add('invalid');
 });
